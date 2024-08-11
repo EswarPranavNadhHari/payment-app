0d33b6d24e42339709181183652aedc85ca0b472
EswarPranavNadhHari <eswarpranavnadhhari@gmail.com>
2024-08-11 20:50:25 +0530

    fix: add skeleton for users list

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,6 +6,8 @@ import { User } from "../components/User"
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import { useDebouncing } from "../hooks/useDebouncing"
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export const Dashboard = () => {
     const [balance, setBalance] = useState("")
@@ -48,25 +50,30 @@ export const Dashboard = () => {
 
     const users = useDebouncing(`http://localhost:3000/api/v1/user/bulk?filter=${search}`);
 
-    return <div className="">
+    return <div>
         <AppBar name={name}></AppBar>
         <div className="px-5 py-5" >
             <div className="font-bold text-2xl">{`Your Balance ₹${balance}`}</div>
             <InputBox onChange={(e)=>{
                 setSearch(e.target.value);
             }} className="font-bold text-2xl py-5" title="Users" type="text" placeHolder="search users"></InputBox>
-            <div className="pt-5">
-                {users.map((user, index)=>{
-                    return <div key={index} className="flex justify-between py-2 items-center">
-                        <User name={user.firstName}></User>
-                        <Button onClick={()=>{
-                            navigate("/SendMoney", {
-                                state: user
-                            });
-                        }} className="w-fit" label={"Send Money"}></Button>
-                    </div>
-                })}
-            </div>
+            <SkeletonTheme height={40} borderRadius={50}>
+              <div className="pt-5">
+                  {users.length > 0 ?
+                  users.map((user, index)=>{
+                      return <div key={index} className="flex justify-between py-2 items-center">
+                          <User name={user.firstName}></User>
+                          <Button onClick={()=>{
+                              navigate("/SendMoney", {
+                                  state: user
+                              });
+                          }} className="w-fit" label={"Send Money"}></Button>
+                      </div>
+                  }) : <Skeleton className="my-3" count={10} /> 
+                  } 
+              </div>
+            </SkeletonTheme>
+            
         </div>
     </div>
 }
