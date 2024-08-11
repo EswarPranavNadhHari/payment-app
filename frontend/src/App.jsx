2d54d4bb0ba6385bf6c521a75a1b8db1e0bb94d2
EswarPranavNadhHari <eswarpranavnadhhari@gmail.com>
2024-08-11 20:36:06 +0530

    fix: modify landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { Signup } from "./pages/Signup.jsx";
 import { Signin } from "./pages/Signin";
 import { SendMoney } from "./pages/SendMoney";
@@ -7,16 +7,21 @@ import { Dashboard } from "./pages/Dashboard";
 
 function App() {
 
+  const loggedIn = localStorage.getItem("token");
+
   return (
     <div>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Navigate to={loggedIn ? "/Dashboard" : "/Signup"} />}></Route>
             <Route path="/Signup" element={<Signup/>}></Route>
             <Route path="/Signin" element={<Signin/>}></Route>
             <Route path="/Dashboard" element={<Dashboard/>}></Route>
             <Route path="/SendMoney" element={<SendMoney/>}></Route>
           </Routes>
         </BrowserRouter>
+
+
     </div>
   )
 }
